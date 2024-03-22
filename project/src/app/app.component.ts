429982af89f66fe4b9c090e857741226ef8fad2c
Arthur Mendes Gomes da Silva <arthur.silva@neovero.com>
2024-03-21 22:39:24 -0300

    aprendizado1

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -3,14 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
 import { FooterComponent } from './src/app/_components/footer/footer.component';
 import { NavbarComponent } from './src/app/_components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { BaseUiComponent } from './src/app/_components/base-ui/base-ui.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FooterComponent, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, FooterComponent, NavbarComponent, AppComponent, CommonModule, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project';
+  exibirHome: boolean = true;
+  ususrios: string[] = ["samira", "lux", "velbeth"];
+  number: number = 0;
+  destruir() {
+    this.exibirHome = !this.exibirHome;
+  }
+  contador() {
+    this.number++
+  }
 }
