293b78b968fb90f6bb4a858c29f26a832fab72a5
Arthur Mendes Gomes da Silva <arthur.silva@neovero.com>
2024-03-24 16:42:56 -0300

    Projeto 1 - Utilizacao do queryParams

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { HomeComponent } from '../pages/home/home.component';
 import { UsersComponent } from '../pages/users/users.component';
 import { NgModel } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { UserComponent } from './src/app/_components/user/user.component';
 
 export const routes: Routes = [
     // {
     //     path: "home",
     //     component : HomeComponent
     // },
-    {
-        path: "user/list",
-        component : UsersComponent
-    }
+    { path: "user/list", component: UsersComponent },
+    { path: "user/:username", component: UserComponent },
+    { path: "user/details", component: UserComponent }
 ];
--- project/src/pages/users/users.component.ts
+++ project/src/pages/users/users.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, UsersComponent],
+  imports: [CommonModule, UsersComponent, RouterModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
